29342648df2e15700ef44ed9dbc72da3bef56bab
German Ovalle Silva <39679249+govalle3@users.noreply.github.com>
2021-03-01 22:58:18 -0500

    User list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo:'/appComponent', pathMatch: 'full'},
-  {path: 'appComponent', component: AppComponent}
+  {path: 'appComponent', component: AppComponent},
+  {path: 'userComponent', component: UserComponent}
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { UserComponent } from './user/user.component';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
- import {RouterModule} from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -12,8 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
