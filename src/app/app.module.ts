348c7d2304a7afd11c6a9a433a9554b8a049d1b7
German Ovalle Silva <39679249+govalle3@users.noreply.github.com>
2021-03-01 22:18:45 -0500

    tables created with bootstrap, user component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
